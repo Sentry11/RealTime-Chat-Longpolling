6db9726815bf58cba6246d73c89b8e414dd4151f
Sentry11 <99908368+Sentry11@users.noreply.github.com>
2022-11-19 19:46:00 +0200

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // React Strict mode comented because in this mode react re-rendered twice as a result
+  // message are duplicated. In prodaction this bug non-exist
   // <React.StrictMode>
     <App />
   // {/* </React.StrictMode> */}
